78d9156f528b2dbdef5869cde4219ffb54c4a207
Javier Aguirre <javieraguirreco10@gmail.com>
2022-08-26 14:05:38 -0400

    Se hicieron modificaciones sobre el archivo .js y se agregó una nota provisoria.

--- funciones.js
+++ funciones.js
@@ -2,36 +2,95 @@ let nombreCliente = prompt('¡Muchísimas gracias por interesarte en mi trabajo 
 alert('¡Un gusto conocerte '+nombreCliente+'!')
 let tipoWeb = prompt('Cuéntame, que tipo de página web necesitas cotizar: \nBlog \neCommerce \nLanding page')
 tipoWeb = tipoWeb.toLocaleLowerCase();
-let totalCotizacion = 0
+let montoBase = 0
+valorSegundaOpcionBlog = 0
+valorTerceraOpcionBlog = 0
+valorCuartaOpcionBlog = 0
 
 while((tipoWeb !== 'blog') && (tipoWeb !== 'ecommerce') && (tipoWeb !== 'landing page')){
-    alert('Por favor, ingresa una de las opciones anteriores.');
+    alert('Por favor, ingresa una de las opciones anteriores.')
     tipoWeb = prompt('Cuéntame, que tipo de página web necesitas cotizar: \nBlog \neCommerce \nLanding page')
-    tipoWeb = tipoWeb.toLocaleLowerCase
+    tipoWeb = tipoWeb.toLocaleLowerCase()
 } 
 
 if(tipoWeb === 'blog'){
-    totalCotizacion = totalCotizacion + 150000
-    primeraOpcionBlog = prompt('¿Sobre qué quieres hablar en tu blog? Elige una opción: \nNoticias \nViajes \nRecetas \nBitácora \nCine y entretenimiento \nOtro');
-    segundaOpcionBlog = prompt('¿Cuantas secciones o categorías tiene tu blog? \nDe 1 a 5 secciones. \nDe 5 a 10 secciones. \nMás de 10 secciones.');
-    terceraOpcionBlog = prompt('¿Cuántas entradas mensuales tendrá tu blog? \nDe 1 a 10 entradas mensuales. \nDe 11 a 20 entradas mensuales. \nMás de 20 entradas mensuales.');
-    cuartaOpcionBlog = prompt('¿Tu blog contará con un foro? \nSí \nNo')
-} else if(tipoWeb === 'ecommerce'){
-    totalCotizacion = totalCotizacion + 250000
-    primeraOpcionEcommerce = prompt('¿Sobre qué es tu marca? \nTecnología \nServicios \nAlimentos \nDeco \nJuguetes \nOtro');
-    segundaOpcionEcommerce = prompt('¿Cuantos productos manejas? \nDe 1 a 20 productos. \nDe 21 a 50 productos. \nDe 51 a 100 productos. \nMás de 100 productos.');
-    terceraOpcionEcommerce = prompt('¿Cuentas con fotografías profesionales de tus productos? \nSí \nNo');
-    cuartaOpcionEcommerce = prompt('¿Tus clientes necesitan loguearse para comprar? \nSí \nNo');
-    quintaOpcionEcommerce = prompt('¿Tus clientes necesitarán un asistente virtual para realizar sus compras? \nSí \nNo');
-    sextaOpcionEcommerce = prompt('¿Tienes varias categorías de productos? \nSí \nNo')
-} else if(tipoWeb === 'landing page'){
-    totalCotizacion = totalCotizacion + 150000
-    primeraOpcionLanding = prompt('¿Que quieres promocionar en tu landing page? \nUn producto \nUn servicio \nUna marca \nUna ONG');
-    segundaOpcionLanding = prompt('¿Necesitas captar datos de los visitantews? \nSí \nNo');
-    terceraOpcionLanding = prompt('¿Necesitas enviarle algún regalo a los visitantes? \nSí \nNo')
+    montoBase = montoBase + 150000
+    primeraOpcionBlog = prompt('¿Sobre qué quieres hablar en tu blog? Elige una opción: \nNoticias \nViajes \nRecetas \nBitácora \nCine y entretenimiento \nOtro')
+    primeraOpcionBlog = primeraOpcionBlog.toLocaleLowerCase()
+    segundaOpcionBlog = prompt('¿Cuantas secciones o categorías tiene tu blog? \nDe 1 a 5 secciones. \nDe 6 a 10 secciones. \nMás de 10 secciones.')
+    segundaOpcionBlog = segundaOpcionBlog.toLocaleLowerCase()
+    if(segundaOpcionBlog === 'de 1 a 5 secciones'){
+        valorSegundaOpcionBlog = valorSegundaOpcionBlog + 50000
+    } else if(segundaOpcionBlog === 'de 6 a 10 secciones'){
+        valorSegundaOpcionBlog = valorSegundaOpcionBlog + 75000
+    } else if(segundaOpcionBlog === 'mas de 10 secciones'){
+        valorSegundaOpcionBlog = valorSegundaOpcionBlog + 100000
+    } terceraOpcionBlog = prompt('¿Cuántas entradas mensuales tendrá tu blog? \nDe 1 a 10 entradas mensuales. \nDe 11 a 20 entradas mensuales. \nMás de 20 entradas mensuales.')
+    terceraOpcionBlog = terceraOpcionBlog.toLocaleLowerCase()
+    if(terceraOpcionBlog === 'de 1 a 10 entradas mensuales'){
+        valorTerceraOpcionBlog = valorTerceraOpcionBlog + 50000
+    } else if(terceraOpcionBlog === 'de 11 a 20 entradas mensuales'){
+        valorTerceraOpcionBlog = valorTerceraOpcionBlog + 75000
+    } else if(terceraOpcionBlog === 'mas de 20 mensuales'){
+        valorTerceraOpcionBlog = valorTerceraOpcionBlog + 100000
+    } cuartaOpcionBlog = prompt('¿Tu blog contará con un foro? \nSí \nNo')
+    cuartaOpcionBlog = cuartaOpcionBlog.toLocaleLowerCase()
+    if(cuartaOpcionBlog === 'si'){
+        valorCuartaOpcionBlog = valorCuartaOpcionBlog + 100000
+    } else{
+        valorCuartaOpcionBlog = 0
+    }
+} 
+
+function sumarblog(){
+    const numero1 = valorSegundaOpcionBlog
+    const numero2 = valorTerceraOpcionBlog
+    const numero3 = valorCuartaOpcionBlog
+    const resultado = montoBase + numero1 + numero2 + numero3
+    return resultado(montoBase + numero1 + numero2 + numero3)
 }
+console.log(sumarblog)
+alert('La cotización de tu página web es '+sumarblog+ ' pesos chilenos, ' +nombreCliente+ '.')
 
-alert('La cotización de tu página web es '+totalCotizacion+ ' pesos chilenos, ' +nombreCliente+ '.')
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+// else if(tipoWeb === 'ecommerce'){
+//     montoBase = montoBase + 250000
+//     primeraOpcionEcommerce = prompt('¿Sobre qué es tu marca? \nTecnología \nServicios \nAlimentos \nDeco \nJuguetes \nOtro')
+//     primeraOpcionEcommerce = primeraOpcionEcommerce.toLocaleLowerCase()
+//     segundaOpcionEcommerce = prompt('¿Cuantos productos manejas? \nDe 1 a 20 productos. \nDe 21 a 50 productos. \nDe 51 a 100 productos. \nMás de 100 productos.')
+//     segundaOpcionEcommerce = segundaOpcionEcommerce.toLocaleLowerCase()
+//     terceraOpcionEcommerce = prompt('¿Cuentas con fotografías profesionales de tus productos? \nSí \nNo')
+//     terceraOpcionEcommerce = terceraOpcionEcommerce.toLocaleLowerCase()
+//     cuartaOpcionEcommerce = prompt('¿Tus clientes necesitan loguearse para comprar? \nSí \nNo')
+//     cuartaOpcionEcommerce = cuartaOpcionEcommerce.toLocaleLowerCase()
+//     quintaOpcionEcommerce = prompt('¿Tus clientes necesitarán un asistente virtual para realizar sus compras? \nSí \nNo')
+//     quintaOpcionEcommerce = quintaOpcionEcommerce.toLocaleLowerCase()
+//     sextaOpcionEcommerce = prompt('¿Tienes varias categorías de productos? \nSí \nNo')
+//     sextaOpcionEcommerce = sextaOpcionEcommerce.toLocaleLowerCase()
+// } 
+
+// else if(tipoWeb === 'landing page'){
+//     montoBase = montoBase + 150000
+//     primeraOpcionLanding = prompt('¿Que quieres promocionar en tu landing page? \nUn producto \nUn servicio \nUna marca \nUna ONG')
+//     primeraOpcionLanding = primeraOpcionLanding.toLocaleLowerCase()
+//     segundaOpcionLanding = prompt('¿Necesitas captar datos de los visitantes? \nSí \nNo')
+//     segundaOpcionLanding = segundaOpcionLanding()
+//     terceraOpcionLanding = prompt('¿Necesitas enviarle algún regalo a los visitantes? \nSí \nNo')
+//     terceraOpcionLanding = terceraOpcionLanding.toLocaleLowerCase()
+// }
+
+// alert('La cotización de tu página web es '+montoBase+ ' pesos chilenos, ' +nombreCliente+ '.')
---
+++ notas.md
@@ -0,0 +1,47 @@
+Blog (montoBase = $150.000)
+---------------------------
+Respuesta 1 = No suma nada.
+
+Respuesta 2 suma: de 1 a 5 secciones = $50.000
+                  de 6 a 10 secciones = $75.000
+	          más de 10 secciones = $100.000
+
+Respuesta 3 suma: de 1 a 10 entradas mensuales = $50.000
+                  de 11 a 20 entradas mensuales = $75.000
+                  Más de 20 entradas mensuales = $100.000
+
+Respuesta 4 suma o no: Sí tiene blog = $100.000
+                       No tiene blog = No suma nada
+
+
+
+Ecommerce (montoBase = $250.000)
+--------------------------------
+Respuesta 1 = No suma nada.
+
+Respuesta 2 suma o no: de 1 a 20 productos = No suma nada
+                       de 21 a 50 productos = $100.000
+                       Más de 50 productos = $150.000
+
+Respuesta 3 suma o no: Sí tiene fotografías = No suma nada
+                       No tiene fotografías = $100.000
+
+Respuesta 4 suma o no: Sí necesitan loguearse = $50.000
+                       No necesitan loguearse = No suma nada
+
+Respuesta 5 suma o no: Sí necesita asistente virtual = $50.000
+                       No necesita asistente virtual = No suma nada
+
+Respuesta 6 = No suma nada.
+
+
+
+Landing page (montoBase = $150.000)
+-----------------------------------
+Respuesta 1 = No suma nada.
+
+Respuesta 2 suma o no: Sí necesita captar datos = $100.000
+                       No necesita captar datos = No suma nada.
+
+Respuesta 3 suma o no: Sí necesita enviar regalos = $50.000
+                       No necesita enviar regalos = No suma nada.
